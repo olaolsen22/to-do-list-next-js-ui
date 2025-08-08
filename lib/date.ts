8687ffa8758b30a6d5d94a294f6fba1b9405d9bd
Ola Talaba Olsen <olatalabaolsen@gmail.com>
2025-08-08 17:13:19 +0200

    refactor(lib): update lib files for new sorting and types

--- lib/date.ts
+++ lib/date.ts
@@ -1,8 +1,18 @@
-export function formatDate(date: Date | string): string {
-  const dateObject = new Date(date);
-  return new Intl.DateTimeFormat('nb-NO', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-  }).format(dateObject);
+import { format, parseISO } from 'date-fns';
+import { nb } from 'date-fns/locale';
+
+// ToDo: include time, format to match local device's date format
+export function formatDate(dateString: string | null | undefined): string {
+  if (!dateString) {
+    return 'No date provided';
+  }
+
+  try {
+    const date = parseISO(dateString);
+    // ToDo: update to match local device's format
+    return format(date, 'dd.MM.yyyy', { locale: nb });
+  } catch (error) {
+    console.error('Invalid date: ', error);
+    return 'Invalid Date';
+  }
 }
--- lib/sort.ts
+++ lib/sort.ts
@@ -3,13 +3,19 @@ import type { ToDoCardProps } from '@/types';
 export function sortToDoListByDate(
   items: Array<ToDoCardProps>,
 ): Array<ToDoCardProps> {
-  const uncompletedItems = items
-    .filter((item) => !item.isCompleted)
-    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  return items.toSorted((a, b) => {
+    if (a.done !== b.done) {
+      return Number(a.done) - Number(b.done);
+    }
 
-  const completedItems = items
-    .filter((item) => item.isCompleted)
-    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-
-  return [...uncompletedItems, ...completedItems];
+    if (a.done === false) {
+      const dateA = new Date(a.created_at ?? 0).getTime();
+      const dateB = new Date(b.created_at ?? 0).getTime();
+      return dateB - dateA;
+    } else {
+      const dateA = new Date(a.completed_on ?? 0).getTime();
+      const dateB = new Date(b.completed_on ?? 0).getTime();
+      return dateB - dateA;
+    }
+  });
 }
